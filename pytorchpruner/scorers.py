5425b4bb1fc9460a461b918ee9a087f1c4025b58
evcu <evcu@users.noreply.github.com>
2018-05-12 01:48:08 -0400

    improvements

--- pytorchpruner/scorers.py
+++ pytorchpruner/scorers.py
@@ -3,7 +3,7 @@ Each scorer should implement score function, which given parameter tensor return
 scores in the same shape. High scores mean high salincies.
 '''
 
-from .utils import hessian_fun,gradient_fun,get_reverse_flatten_params_fun,hessian_vector_product
+from .utils import hessian_fun,gradient_fun,get_reverse_flatten_params_fun,hessian_vector_product,flatten_params
 from torch import nn
 import collections
 import torch
@@ -32,14 +32,21 @@ def taylor1Scorer(params,loss,w_fun=lambda a: -a):
     elif isinstance(params,collections.Iterable):
         params = list(params)
         grads = gradient_fun(loss,params,retain_graph=True)
-        result = list(map(lambda w,g:torch.mul(w_fun(w.data),g),zip(params,grads)))
+        result = [torch.mul(w_fun(w.data),g.data) for w,g in zip(params,grads)]
     else:
         raise ValueError("Invalid type, received: %s. either supply iterable of \
                             parameters or a single parameter" % type(params))
     return result
 
 def taylor1ScorerAbs(params,loss,w_fun=lambda a:-a):
-    return taylor1Scorer(params,loss,w_fun=w_fun).abs()
+    if isinstance(params,nn.Parameter):
+        return taylor1Scorer(params,loss,w_fun=w_fun).abs()
+    elif isinstance(params,collections.Iterable):
+        return [s.abs() for s in taylor1Scorer(params,loss,w_fun=w_fun)]
+    else:
+        raise ValueError("Invalid type, received: %s. either supply iterable of \
+                            parameters or a single parameter" % type(params))
+
 
 
 def hessianScorer(params,loss,w_fun=lambda a:-a):
@@ -68,7 +75,7 @@ def hessianScorer(params,loss,w_fun=lambda a:-a):
         vector = flatten_params((w_fun(p.data.clone()) for p in params))
         flat_hv = hessian_vector_product(loss,params,vector,retain_graph=True,flattened=True)
         hv = rev_f(flat_hv)
-        result = list(map(lambda w,h:torch.mul(w_fun(w.data),h),zip(params,hv)))
+        result = [torch.mul(w_fun(w.data),h) for w,h in zip(params,hv)]
     else:
         raise ValueError("Invalid type, received: %s. either supply iterable of \
                             parameters or a single parameter" % type(params))
@@ -76,7 +83,13 @@ def hessianScorer(params,loss,w_fun=lambda a:-a):
     return result
 
 def hessianScorerAbs(params,loss,w_fun=lambda a:-a):
-    return hessianScorer(params,loss,w_fun=w_fun).abs()
+    if isinstance(params,nn.Parameter):
+        return hessianScorer(params,loss,w_fun=w_fun).abs()
+    elif isinstance(params,collections.Iterable):
+        return [s.abs() for s in hessianScorer(params,loss,w_fun=w_fun)]
+    else:
+        raise ValueError("Invalid type, received: %s. either supply iterable of \
+                            parameters or a single parameter" % type(params))
 
 def taylor2Scorer(params,loss,w_fun=lambda a: -a,scale=1):
     """
@@ -98,32 +111,56 @@ def taylor2Scorer(params,loss,w_fun=lambda a: -a,scale=1):
         flat_hv = hessian_vector_product(loss,params,vector,retain_graph=True,flattened=True)
         hv = rev_f(flat_hv)
         grads = gradient_fun(loss,params,retain_graph=True)
-        result = list(map(lambda w,h,g:torch.mul(w_fun(w.data),scale*h+g),zip(params,hv,grads)))
+        result = [torch.mul(w_fun(w.data),scale*h+g.data) for w,h,g in zip(params,hv,grads)]
     else:
         raise ValueError("Invalid type, received: %s. either supply iterable of \
                             parameters or a single parameter" % type(params))
     return result
 
 def taylor2ScorerAbs(params,loss,w_fun=lambda a:-a,scale=1):
-    return taylor2Scorer(params,loss,w_fun=w_fun,scale=scale).abs()
+    if isinstance(params,nn.Parameter):
+        return taylor2Scorer(params,loss,w_fun=w_fun,scale=scale).abs()
+    elif isinstance(params,collections.Iterable):
+        return [s.abs() for s in taylor2Scorer(params,loss,w_fun=w_fun,scale=scale)]
+    else:
+        raise ValueError("Invalid type, received: %s. either supply iterable of \
+                            parameters or a single parameter" % type(params))
 
 def lossChangeScorer(params,loss,loss_calc_f=None):
     if loss_calc_f is None:
         raise ValueError(f'loss_calc_f:{loss_calc_f} cannot be None')
-    else:
+
+    if isinstance(params,nn.Parameter):
         scores = params.data.clone()
         for idx in product(*map(range,scores.size())):
             old_val,params.data[idx] = params.data[idx],0
-            scores[idx] = loss_calc_f()[0].data[0]-loss.data[0]
+            scores[idx] = loss_calc_f()-loss.data[0]
             params.data[idx] = old_val
+    elif isinstance(params,collections.Iterable):
+        scores = [lossChangeScorer(p,loss,loss_calc_f=loss_calc_f) for p in params]
+    else:
+        raise ValueError("Invalid type, received: %s. either supply iterable of \
+                            parameters or a single parameter" % type(params))
+
     return scores
 
-def lossChangeScorerAbs(*args,**kwargs):
-    scores = lossChangeScorer(*args,**kwargs)
-    return scores.abs()
+def lossChangeScorerAbs(params,loss,**kwargs):
+    if isinstance(params,nn.Parameter):
+        return lossChangeScorer(params,loss,**kwargs).abs()
+    elif isinstance(params,collections.Iterable):
+        return [s.abs() for s in lossChangeScorer(params,loss,**kwargs)]
+    else:
+        raise ValueError("Invalid type, received: %s. either supply iterable of \
+                            parameters or a single parameter" % type(params))
 
 def randomScorer(params,loss):
     old_state = torch.get_rng_state()
-    res = torch.rand(params.size())
+    if isinstance(params,nn.Parameter):
+        res = torch.rand(params.size())
+    elif isinstance(params,collections.Iterable):
+        res = [randomScorer(p,loss) for p in params]
+    else:
+        raise ValueError("Invalid type, received: %s. either supply iterable of \
+                            parameters or a single parameter" % type(params))
     torch.set_rng_state(old_state)
     return res
